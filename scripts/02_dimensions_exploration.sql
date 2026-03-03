3bd90cd17e5587947856d781ad4fc9c7a3f0d449
CodingandData <meadowriverridge@gmail.com>
2026-03-03 04:01:03 -0500

    Update and rename Dimensions Exploration.sql to 02_dimensions_exploration.sql

--- scripts/02_dimensions_exploration.sql
+++ scripts/02_dimensions_exploration.sql
@@ -4,4 +4,5 @@ SELECT DISTINCT country FROM gold.dim_customers
 
 -- Explore all categories "The major divisions"
 SELECT DISTINCT category, subcategory, product_name FROM gold.dim_products
+
 ORDER BY 1,2,3
